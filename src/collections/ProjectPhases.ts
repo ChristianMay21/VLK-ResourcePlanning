a0e96cbe159ffc04cdb6995612d67b33a33b2345
Christian May <christianmay21@gmail.com>
2026-07-08 16:08:14 -0500

    Added two new collection configs

---
+++ src/collections/ProjectPhases.ts
@@ -0,0 +1,25 @@
+import type { CollectionConfig } from 'payload'
+
+export const ProjectPhases: CollectionConfig = {
+  slug: 'project-phases',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'startDate',
+      type: 'date',
+      required: true,
+    },
+    {
+      name: 'endDate',
+      type: 'date',
+      required: true,
+    },
+  ],
+}
