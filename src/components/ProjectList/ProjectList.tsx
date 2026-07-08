326d3064fcecccf5595c429734ae1eed284e4531
Christian May <christianmay21@gmail.com>
2026-07-08 16:50:19 -0500

    Completed most basic project list view

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { getPayload } from 'payload'
 import config from '@/payload.config'
+import type { Client } from '@/payload-types'
 import ProjectListEntry from '@/components/ProjectListEntry/ProjectListEntry'
 import styles from './ProjectList.module.scss'
 
@@ -7,12 +8,22 @@ type ProjectListProps = {}
 
 export default async function ProjectList(props: ProjectListProps) {
   const payload = await getPayload({ config: await config })
-  const { docs } = await payload.find({ collection: 'projects' })
+  const { docs } = await payload.find({ collection: 'projects', depth: 1 })
 
   return (
     <div className={styles.root}>
+      <span className={styles.heading}>Project</span>
+      <span className={styles.heading}>Client</span>
+      <span className={styles.heading}>Start Date</span>
+      <span className={styles.heading}>End Date</span>
       {docs.map((project) => (
-        <ProjectListEntry key={project.id} name={project.name} />
+        <ProjectListEntry
+          key={project.id}
+          name={project.name}
+          clientName={typeof project.client === 'object' ? (project.client as Client).name : ''}
+          startDate={project.startDate}
+          endDate={project.endDate}
+        />
       ))}
     </div>
   )
--- src/components/ProjectListEntry/ProjectListEntry.tsx
+++ src/components/ProjectListEntry/ProjectListEntry.tsx
@@ -2,8 +2,18 @@ import styles from './ProjectListEntry.module.scss'
 
 type ProjectListEntryProps = {
   name: string
+  clientName: string
+  startDate: string
+  endDate: string
 }
 
 export default function ProjectListEntry(props: ProjectListEntryProps) {
-  return <div className={styles.root}>{props.name}</div>
+  return (
+    <div className={styles.root}>
+      <span className={styles.name}>{props.name}</span>
+      <span className={styles.client}>{props.clientName}</span>
+      <span className={styles.date}>{new Date(props.startDate).toLocaleDateString()}</span>
+      <span className={styles.date}>{new Date(props.endDate).toLocaleDateString()}</span>
+    </div>
+  )
 }
