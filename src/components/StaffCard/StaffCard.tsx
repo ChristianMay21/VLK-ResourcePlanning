2f79087b604a845defd20624023957e74dbbfe43
Christian May <christianmay21@gmail.com>
2026-07-08 18:19:41 -0500

    Got simple staff grid working

--- src/components/StaffCard/StaffCard.tsx
+++ src/components/StaffCard/StaffCard.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
 import styles from './StaffCard.module.scss'
 
-type StaffCardProps = {}
+type StaffCardProps = {
+  photoUrl: string
+  name: string
+  jobTitle: string
+}
 
 export default function StaffCard(props: StaffCardProps) {
-  return <div className={styles.root}></div>
+  return (
+    <div className={styles.root}>
+      {props.photoUrl && (
+        <Image className={styles.photo} src={props.photoUrl} alt={props.name} width={80} height={80} />
+      )}
+      <p className={styles.name}>{props.name}</p>
+      <p className={styles.jobTitle}>{props.jobTitle}</p>
+    </div>
+  )
 }
--- src/components/StaffGrid/StaffGrid.tsx
+++ src/components/StaffGrid/StaffGrid.tsx
@@ -1,7 +1,25 @@
+import { getPayload } from 'payload'
+import config from '@/payload.config'
+import type { Media } from '@/payload-types'
+import StaffCard from '@/components/StaffCard/StaffCard'
 import styles from './StaffGrid.module.scss'
 
 type StaffGridProps = {}
 
-export default function StaffGrid(props: StaffGridProps) {
-  return <div className={styles.root}></div>
+export default async function StaffGrid(props: StaffGridProps) {
+  const payload = await getPayload({ config: await config })
+  const { docs } = await payload.find({ collection: 'employees', depth: 1 })
+
+  return (
+    <div className={styles.root}>
+      {docs.map((employee) => (
+        <StaffCard
+          key={employee.id}
+          name={employee.name}
+          jobTitle={employee.jobTitle ?? ''}
+          photoUrl={typeof employee.photo === 'object' && employee.photo ? (employee.photo as Media).url ?? '' : ''}
+        />
+      ))}
+    </div>
+  )
 }
